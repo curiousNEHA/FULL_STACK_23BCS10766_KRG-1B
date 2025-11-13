1a2f42ef041e26642ab82c4801c14232422b3059
Neha Sharma <155069649+curiousNEHA@users.noreply.github.com>
2025-11-13 10:37:31 +0530

    Update userEntity.java

--- FINAL-PRACTICAL/QUESTION4/userEntity.java
+++ FINAL-PRACTICAL/QUESTION4/userEntity.java
@@ -1,1 +1,24 @@
+package com.example.demo.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class userEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String Email;
+    private String Salary;
+
+    
+} 
